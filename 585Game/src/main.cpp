55a6dbad3d735b493d97e2b9c115899343f0bd3f
Peter Bignold <noodlegamesdev@gmail.com>
2023-01-13 13:20:29 -0700

    Added GLFW Basics
    A super basic GLFW window. GLM isn't set up yet, that will be the next step!
    
    NOTE: To run after builds, you will need to include glfw3.dll in the x64/Debug and x64/Release folders. The dll is now in the google drive folder.

--- 585Game/src/main.cpp
+++ 585Game/src/main.cpp
@@ -1,90 +1,51 @@
+// Core Includes
 #include <iostream>
 
-#include "PxPhysicsAPI.h"
+// 3rd Party Includes
+#include <PxPhysicsAPI.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
-int main()
-{
-	//PhysX management class instances.
-	physx::PxDefaultAllocator gAllocator;
-	physx::PxDefaultErrorCallback gErrorCallback;
-	physx::PxFoundation* gFoundation = NULL;
-	physx::PxPhysics* gPhysics = NULL;
-	physx::PxDefaultCpuDispatcher* gDispatcher = NULL;
-	physx::PxScene* gScene = NULL;
-	physx::PxMaterial* gMaterial = NULL;
-	physx::PxPvd* gPvd = NULL;
+using namespace std;
 
-	// Initialize PhysX
-	gFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gAllocator, gErrorCallback);
-	if (!gFoundation)
-	{
-		std::cout << "PxCreateFoundation failed!" << std::endl;
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+int main() {
+
+	const int SCREEN_X = 1440;
+	const int SCREEN_Y = 1440;
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SCREEN_X, SCREEN_Y, "585 Game", NULL, NULL);
+
+	if (window == NULL) {
+		cout << "Failed To Create Window\n";
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize GLAD\n";
 		return -1;
 	}
 
-	// PVD
-	gPvd = PxCreatePvd(*gFoundation);
-	physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-	gPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
-
-	// Physics
-	gPhysics = PxCreatePhysics(PX_PHYSICS_VERSION, *gFoundation, physx::PxTolerancesScale(), true, gPvd);
-	if (!gPhysics)
+	while (!glfwWindowShouldClose(window))
 	{
-		std::cout << "PxCreatePhysics failed!" << std::endl;
-		return -1;
+		glfwSwapBuffers(window);
+		glfwPollEvents();
 	}
 
-	// Scene
-	physx::PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-	sceneDesc.gravity = physx::PxVec3(0.0f, -9.81f, 0.0f);
-	gDispatcher = physx::PxDefaultCpuDispatcherCreate(2);
-	sceneDesc.cpuDispatcher = gDispatcher;
-	sceneDesc.filterShader = physx::PxDefaultSimulationFilterShader;
-	gScene = gPhysics->createScene(sceneDesc);
+	glViewport(0, 0, SCREEN_X, SCREEN_Y);
 
-	// Prep PVD
-	physx::PxPvdSceneClient* pvdClient = gScene->getScenePvdClient();
-	if (pvdClient)
-	{
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-		pvdClient->setScenePvdFlag(physx::PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-	}
-
-	// Simulate
-	gMaterial = gPhysics->createMaterial(0.5f, 0.5f, 0.6f);
-	physx::PxRigidStatic* groundPlane = physx::PxCreatePlane(*gPhysics, physx::PxPlane(0, 1, 0, 50), *gMaterial);
-	gScene->addActor(*groundPlane);
-
-	// Define a box
-	float halfLen = 0.5f;
-	physx::PxShape* shape = gPhysics->createShape(physx::PxBoxGeometry(halfLen, halfLen, halfLen), *gMaterial);
-	physx::PxU32 size = 30;
-	physx::PxTransform tran(physx::PxVec3(0));
-
-	// Create a pyramid of physics-enabled boxes
-	for (physx::PxU32 i = 0; i < size; i++)
-	{
-		for (physx::PxU32 j = 0; j < size - i; j++)
-		{
-			physx::PxTransform localTran(physx::PxVec3(physx::PxReal(j * 2) - physx::PxReal(size - i), physx::PxReal(i * 2 - 1), 0) * halfLen);
-			physx::PxRigidDynamic* body = gPhysics->createRigidDynamic(tran.transform(localTran));
-			body->attachShape(*shape);
-			physx::PxRigidBodyExt::updateMassAndInertia(*body, 10.0f);
-			gScene->addActor(*body);
-		}
-	}
-
-	// Clean up
-	shape->release();
-
-	// Simulate at 60fps
-	while (1)
-	{
-		gScene->simulate(1.0f / 60.0f);
-		gScene->fetchResults(true);
-	}
-
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }
