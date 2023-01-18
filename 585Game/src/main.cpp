4a297e26cc8d1558b5537ab21538d11f659d01b3
Peter Lefaivre <noodlegamesdev@gmail.com>
2023-01-17 18:03:28 -0700

    Added Timer Class
    Keeps track of Delta Time and FPS (Frames Per Second). Implements a singleton class structure to ensure only one instance of Timer can exist, helping to ensure time-sensitive operations are always synchronized.
    
    main.cpp grabs a pointer to the singleton instance, then each frame updates Delta Time and FPS.

--- 585Game/src/main.cpp
+++ 585Game/src/main.cpp
@@ -11,6 +11,7 @@
 #include <Boilerplate/Window.h>
 #include <Boilerplate/Input.h>
 #include <Boilerplate/Shader.h>
+#include <Boilerplate/Timer.h>
 
 using namespace std;
 using namespace glm;
@@ -28,15 +29,25 @@ int main() {
 			 0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
 			 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
 	};
+
 	//Vertex buffer initialization
 	Shader basicShader("src/Boilerplate/shaderVertex.txt","src/Boilerplate/shaderFragment.txt");
 	unsigned int testTriangleVAO = initVAO(testTriangle, sizeof(testTriangle));
+
+	// Time initialization
+	Timer &timer = Timer::Instance();		// Create pointer to singleton timer instance
 
 	// PRIMARY GAME LOOP
 	while (!glfwWindowShouldClose(window)) {
 		// Process GLFW window user inputs
 		processInput(window);
-		
+
+		// Update time
+		timer.update();								// Update time instance
+		double deltaTime = timer.getDeltaTime();	// Get delta time
+		int fps = timer.getFPS(0.2);				// Get fps (WARNING: can be NULL!)
+		if (fps != NULL) cout << fps << "\n";		// TODO: Render FPS on screen instead of printing to console
+
 		// <game stuff goes here>
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
