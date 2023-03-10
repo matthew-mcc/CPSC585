e9c686ddfe95c0b73f890cee40d50e8707718e96
Yifan X <35789104+yxie364@users.noreply.github.com>
2023-03-10 09:52:35 -0700

    Improved dropoff audio accuracy

--- 585Game/src/AudioManager.cpp
+++ 585Game/src/AudioManager.cpp
@@ -52,13 +52,14 @@ void AudioManager::Latch(glm::vec3 pos) {
 void AudioManager::Dropoff(glm::vec3 pos) {
 	//float distance = glm::distance(playerPos, glm::vec3(1.0f, 0.5f, 33.0f));
 	//audioEngine.SetEventParameter(e_dropoff, p_distance, distance);
-
+	glm::vec3 dropoffPos = glm::vec3(1.0f, 0.5f, 33.0f);
+	dropoffPos = pos - dropoffPos;
 	//float result;
 	//audioEngine.SetEventParameter(e_dropoff, "Distance", distance);
 	//audioEngine.SetChannel3dPosition(0, glm::vec3(1.0f, 0.5f, 33.0f));
 	//audioEngine.PlayEvent(e_dropoff);
 
-	audioEngine.PlaySound("assets/audio/ping_placeholder.flac", pos, 10.0f);
+	audioEngine.PlaySound("assets/audio/ping_placeholder.flac", dropoffPos, 10.0f);
 	
 	//std::cout << distance << std::endl;
 	//audioEngine.GetEventParameter(e_dropoff, "parameter: / Distance", &distance);
